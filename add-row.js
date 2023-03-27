1a9a0f78e2332f8f6c052ac562f737b8fa13e70b
Vinci Wu <wu.vinci00@gmail.com>
2023-03-27 17:53:08 -0400

    Removed definition of function and left a skeleton code

--- add-row.js
+++ add-row.js
@@ -1,6 +1,3 @@
 function addRow() {
-    let table = document.getElementById("workoutRecord");
-    let row = table.insertRow(0);
-    let column = table.insertCell(0);
-    column.innerHTML = "Hello";
+    
 }
