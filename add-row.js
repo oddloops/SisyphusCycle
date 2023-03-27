678ff62a08596ef20e5ae9c3e6afc9eef662f4ca
Vinci Wu <wu.vinci00@gmail.com>
2023-03-27 19:13:18 -0400

    partially defined the function to add a row
    - currently only adds in an empty row

--- add-row.js
+++ add-row.js
@@ -1,3 +1,10 @@
-function addRow() {
+const addRowId = document.getElementById('addRow');
+const workoutTable = document.getElementById('tableBody');
+
+addRowId.addEventListener("click", function() {
+    const newRow = workoutTable.insertRow(-1);
     
-}
+    for (let i = 0; i < 7; i++) {
+        const newCell = newRow.insertCell(i);
+    }
+});
