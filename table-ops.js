49f2844f15e1a3107a4e93ff655a81436939fbe6
Vinci Wu <wu.vinci00@gmail.com>
2023-03-27 23:37:41 -0400

    Added comments and a new eventListener to delete the bottom most row

--- table-ops.js
+++ table-ops.js
@@ -1,25 +1,33 @@
-const addRowId = document.getElementById('addRow');
-const workoutTable = document.getElementById('tableBody');
+// get the id of the add row button (+)
+const addRowId = document.getElementById("addRow");
 
+// get the id of the table we want to modify
+const workoutTable = document.getElementById("tableBody");
+
+// add eventListener when the button is clicked to add a row
 addRowId.addEventListener("click", function() {
+    // adds a row to the bottom of the table
     const newRow = workoutTable.insertRow(-1);
     
     for (let i = 0; i < 7; i++) {
         const newCell = newRow.insertCell(i);
         let newData = document.createElement("input");
-        if (i < 2) {
+        if (i < 2) { // workout name and body part (text)
             newData.type = "text";
-        } else if (i < 6) {
+        } else if (i < 6) { // weights, reps, and sets (numbers)
             newData.type = "number";
             newData.required = true;
-        } else {
+        } else { // data achieved (date)
             newData.type = "date";
         }
-        newCell.append(newData);
+        newCell.append(newData); // add the data into their respective column
     }
-    let button = document.createElement("button");
-    button.type = "button";
-    button.id = "delRow";
-    button.innerHTML = "-";
-    newRow.append(button);
+});
+
+// get the id for the delete row button (-)
+const delRowId = document.getElementById("delRow");
+
+// adds eventlistener to delete the bottommost row
+delRowId.addEventListener("click", function() {
+   workoutTable.deleteRow(-1);
 });
