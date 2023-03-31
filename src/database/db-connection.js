18ab2b0309f5613b26d8fa1950502ee63c1cb32b
Vinci Wu <wu.vinci00@gmail.com>
2023-03-31 10:55:02 -0400

    Created db connection file (NOT WORKING)

---
+++ src/database/db-connection.js
@@ -0,0 +1,22 @@
+const mySql = require('mysql');
+const dotenv = require('dotenv');
+
+dotenv.config(); // get environment variables
+
+// Create connection to desired database
+const connection = mySql.createConnection({
+    host: process.env.DB_HOSTNAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+connection.connect((error) => {
+    if (error) {
+        console.error(error.stack);
+        return;
+    }
+    console.log("Connected to mySql GymPanda");
+});
+
+module.exports = connection;
