e90c3223456163cc427c0277f712ed7699ad1840
Vinci Wu <wu.vinci00@gmail.com>
2023-04-03 22:13:47 -0400

    Created js page to hold all functions for the main page

---
+++ public/scripts/main-page.js
@@ -0,0 +1,6 @@
+// login button on main page
+const loginButton = document.getElementById("login-btn");
+
+loginButton.addEventListener('click', () => {
+    window.location.href = '/login.html';
+});
