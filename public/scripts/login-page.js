b40a5f270d6fb06fca47cba45684f19f08ac8778
Vinci Wu <wu.vinci00@gmail.com>
2023-04-03 22:13:05 -0400

    Created js file to hold all operations for the login page

---
+++ public/scripts/login-page.js
@@ -0,0 +1,6 @@
+// sign up button on login page
+const toSignUp = document.getElementById("to-signup");
+
+toSignUp.addEventListener('click', () => {
+    window.location.href = '/signup.html';
+});
