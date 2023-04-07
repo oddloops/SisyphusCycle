6d82346a54573e65f5c8c704960fc69bc7a9dd41
Vinci Wu <wu.vinci00@gmail.com>
2023-04-06 20:05:35 -0400

    Added conditional to ensure that the login button is present

--- public/scripts/main-page.js
+++ public/scripts/main-page.js
@@ -1,6 +1,8 @@
 // login button on main page
 const loginButton = document.getElementById("login-btn");
 
-loginButton.addEventListener('click', () => {
-    window.location.href = '/login';
-});
+if (loginButton) {
+    loginButton.addEventListener('click', () => {
+        window.location.href = '/login';
+    });
+}
