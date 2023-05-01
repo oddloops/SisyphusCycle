732610f2d1d58c65b6382661f08da3851bed770a
Vinci Wu <wu.vinci00@gmail.com>
2023-04-30 21:49:40 -0400

    Added event handler for the logout button to send a post request to the server

--- public/scripts/main-page.js
+++ public/scripts/main-page.js
@@ -7,9 +7,40 @@ if (loginButton) {
     });
 }
 
+const logoutButton = document.getElementById("logout-btn");
+if (logoutButton) {
+    logoutButton.addEventListener('click', () => {
+        console.log('clicked');
+        fetch('/logout', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({})
+        })
+        .then(response => {
+            if (response.ok){
+                window.location.href = '/';
+            } else {
+                console.log("Something went wrong");
+            }
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    });
+}
+
 const historyButton = document.getElementById("history-btn");
 if (historyButton) {
     historyButton.addEventListener('click', () => {
         window.location.href = '/history';
+    });
+}
+
+const profileButton = document.getElementById("profile-btn");
+if (profileButton) {
+    profileButton.addEventListener('click', () => {
+        window.location.href = '/profile';
     });
 }
