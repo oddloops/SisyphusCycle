d3d71adce2cd724d13e13d22c5f66d6ba7addcff
Vinci Wu <wu.vinci00@gmail.com>
2023-05-03 20:56:08 -0400

    Added firebase-auth skeleton for use of firebase system

---
+++ src/database/firebase-auth.js
@@ -0,0 +1,28 @@
+// Firebase Custom Authentication System
+const admin = require("firebase-admin");
+
+const serviceAccount = {
+  "type": process.env.FIREBASE_TYPE,
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "client_id": process.env.FIREBASE_CLIENT_ID,
+  "auth_uri": process.env.FIREBASE_AUTH_URI,
+  "token_uri": process.env.FIREBASE_TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_URL,
+  "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT_URL
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+getAuth()
+  .createCustomToken(uid)
+  .then((customToken) => {
+    // Send token back to client
+  })
+  .catch((error) => {
+    console.log('Error creating custom token:', error);
+  });
