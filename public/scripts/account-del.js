fac353c03a5752052c85e09a6ba6f4301d62a992
Vinci Wu <wu.vinci00@gmail.com>
2023-05-06 23:55:20 -0400

    Created an event listener for when the delete account button is pressed to send a delete request to the server

--- public/scripts/account-del.js
+++ public/scripts/account-del.js
@@ -1,5 +1,26 @@
 const delAccountBtn = document.getElementById("del-account-btn");
 
 delAccountBtn.addEventListener('click', () => {
-    console.log("Del clicked");
+    const askAccountDel = confirm("Delete account?");
+    if (askAccountDel) {
+        fetch('/delAccount', {
+            method: 'DELETE',
+            header: {
+                'Content-Type': 'application/JSON'
+            },
+            body: JSON.stringify({})
+        })
+        .then(response => {
+            if (response.ok) {
+                console.log("Deleted acount");
+                window.location.href = '/';
+            } else {
+                console.error("Error deleting account");
+                throw new Error("Error deleting account");
+            }
+        })
+        .catch(error => {
+            console.error(`Error deleting row: ${error}`);
+        });
+    }
 });
